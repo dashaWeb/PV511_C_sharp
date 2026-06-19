22e078ec753f0f04f580c4e8924a341c22d71810
Dasha <dashakonopelko@gmail.com>
2026-06-19 20:42:53 +0300

    add

--- 13_Inheritance/Program.cs
+++ 13_Inheritance/Program.cs
@@ -1,10 +1,15 @@
 ﻿using _13_Inheritance;
+using System.Text;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         //Device device = new Device();
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.BackgroundColor = ConsoleColor.DarkGray;
+        Console.OutputEncoding = Encoding.Unicode;
+        Console.InputEncoding = Encoding.Unicode;
         TV tv = new TV("LG");
         tv.Price = 44;
         tv.Weight = 333;
