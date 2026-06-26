9f619242c0755345ed43264d4f0809de8c30af50
Dasha <dashakonopelko@gmail.com>
2026-06-26 19:43:56 +0300

    struct

---
+++ 19_Extension methods/Program.cs
@@ -0,0 +1,31 @@
+﻿//class MyString : String
+//{
+
+//}
+
+static class IntExtesion
+{
+    public static int CountDigits(this int number)
+    {
+        return Math.Abs(number).ToString().Length;
+    }
+}
+
+static class ArrayExtesion
+{
+    public static int SumPositive(this int[] arr)
+    {
+        return Array.FindAll(arr, x => x > 0).Sum();
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int value = -12341546;
+        Console.WriteLine($"{value} has {value.CountDigits()}");
+        int[] arr = { 1, 2, 3, -4, -5 };
+        Console.WriteLine(arr.SumPositive());
+    }
+}
---
+++ 21_Struct/City.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_Struct
+{
+    struct City : IComparable, IComparable<City>
+    {
+        const int DefaultPopulation = 100_000;
+        const int MaxPopulation = 10_000_000;
+        private int population;
+        public string Name { get; set; } //= "Noname" error;
+        public int Population
+        {
+            get => population;
+            set => population = value <= MaxPopulation ? value : MaxPopulation;
+            
+        }
+        public City(string name, int population = DefaultPopulation)
+            :this()
+        {
+            Name = name;
+            this.Population = population;
+        }
+        public override string ToString()
+        {
+            return $"City : {Name,-10} Population : {population,-15}";
+        }
+
+        public int CompareTo(object? obj) // not productive boxing unboxing
+        {
+            Console.WriteLine("CompareTo");
+            if (!(obj is City)) // not City
+                throw new ArgumentException("CompareTo() : object is not City");
+            City city = (City)obj; // unboxing
+            //if (this.population < city.population)
+            //    return -1;
+            //if (this.population > city.population)
+            //    return 1;
+            //return 0;
+
+            //return this.population - city.population;
+            return this.population.CompareTo(city.population);
+        }
+
+        public int CompareTo(City other)
+        {
+            Console.WriteLine("CompareTo<>");
+            return this.population.CompareTo(other.population);
+        }
+    }
+
+    struct ComparerByName : IComparer<City>
+    {
+        public int Compare(City x, City y)
+        {
+            return x.Name.CompareTo(y.Name);
+        }
+    }
+    struct ComparerByNameDesc : IComparer<City>
+    {
+        public int Compare(City x, City y)
+        {
+            return -x.Name.CompareTo(y.Name);
+        }
+    }
+}
